7c064f35f88321d23c2f288cef6fa69670623f39
mark.vido <vidomark42@gmail.com>
2025-01-22 17:21:47 +0100

    Ghostty configuration

--- home/programs/ghostty/ghostty.nix
+++ home/programs/ghostty/ghostty.nix
@@ -1,6 +1,28 @@
 { config, pkgs, ... }:
 
 {
+  programs.ghostty = {
+    enable = false;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+    installVimSyntax = true;
+    installBatSyntax = true;
+    settings = {
+      theme = "gruvbox-material";
+      font-family = "Monaspace Neon";
+      font-size = 30;
+      background-opacity = 0.8;
+      background-blur-radius = 70;
+      window-padding-y = 10;
+      window-padding-x = 10;
+      window-decoration = false;
+      macos-option-as-alt = true;
+      macos-titlebar-style = "hidden";
+      macos-window-shadow = true;
+    };
+  };
+
+  # Uncomment this, once the Nix package is fixed
   xdg.configFile.ghostty = {  
     source = ./config;
     recursive = true;

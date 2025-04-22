38e1be719221439eb0a4d54ab21d6040537b1e12
vido.mark <vidomark42@gmail.com>
2025-04-22 10:09:16 +0200

    fzf-tab configuration

--- modules/home-manager/apps/zsh.nix
+++ modules/home-manager/apps/zsh.nix
@@ -31,8 +31,6 @@
       size = 1000000000;
     };
     initExtra = ''
-      bindkey '^ ' autosuggest-accept
-
       ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
 
       zvm_after_init() {
@@ -43,6 +41,25 @@
           source "${pkgs.fzf}/share/fzf/completion.zsh"
         fi
       }
+
+      zstyle ':completion:*:git-checkout:*' sort false
+      # set descriptions format to enable group support
+      # NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+      zstyle ':completion:*:descriptions' format '[%d]'
+      # set list-colors to enable filename colorizing
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+      # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+      zstyle ':completion:*' menu no
+      # preview directory's content with eza when completing cd
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+      # custom fzf flags
+      # NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+      # To make fzf-tab follow FZF_DEFAULT_OPTS.
+      zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+      # NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+      zstyle ':fzf-tab:*' use-fzf-default-opts yes
+      # switch group using `<` and `>`
+      zstyle ':fzf-tab:*' switch-group '<' '>'
 
       if [ -z "$INTELLIJ_ENVIRONMENT_READER" ] && command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
         exec tmux

547f38b13eeea297469fca3036107ac3d5f406be
vido.mark <vidomark42@gmail.com>
2025-06-28 14:06:54 +0200

    Motion blur cursor color change

--- modules/home-manager/apps/ghostty/config/shaders/cursor_smear.glsl
+++ modules/home-manager/apps/ghostty/config/shaders/cursor_smear.glsl
@@ -60,7 +60,7 @@ float ease(float x) {
     return pow(1.0 - x, 3.0);
 }
 
-const vec4 TRAIL_COLOR = vec4(1., 1., 0., 1.0);
+const vec4 TRAIL_COLOR = vec4(0.835, 0.769, 0.631, 1.0);
 const float OPACITY = 0.6;
 const float DURATION = 0.3; //IN SECONDS
 

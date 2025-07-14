cf1151a0e968cf0ea302bdced4cfb613b44bc342
vido.mark <vidomark42@gmail.com>
2025-07-14 16:20:42 +0200

    Starship config changes

--- modules/home-manager/apps/starship.nix
+++ modules/home-manager/apps/starship.nix
@@ -1,14 +1,30 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  noRuntimeVersionsToml =
+    pkgs.runCommand "no-runtime-versions" { nativeBuildInputs = [ pkgs.starship ]; }
+      ''
+        ${pkgs.starship}/bin/starship preset no-runtime-versions -o $out
+      '';
+  noRuntimeVersionsPreset = builtins.fromTOML (builtins.readFile noRuntimeVersionsToml);
+
+  nerdfontToml = pkgs.runCommand "nerd-font-symbols" { nativeBuildInputs = [ pkgs.starship ]; } ''
+    ${pkgs.starship}/bin/starship preset nerd-font-symbols -o $out
+  '';
+  nerdfontPreset = builtins.fromTOML (builtins.readFile nerdfontToml);
+in
 {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
-    settings = {
-      add_newline = false;
-      directory = {
-        truncate_to_repo = false;
+    settings =
+      nerdfontPreset
+      // noRuntimeVersionsPreset
+      // {
+        add_newline = false;
+        directory = {
+          truncate_to_repo = false;
+        };
       };
-    };
   };
 }

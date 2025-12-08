3f430ce372fc8d87bafa3205104d35df4d5edd29
vidomark42 <mark.vido@wise.com>
2025-12-08 19:39:14 +0100

    refactor: convert ideavim, nvim, ghostty to proper NixOS modules
    Add options.apps.* namespace with enable option to each module:
    
    - ideavim: apps.ideavim.enable (default: true)
    - nvim: apps.nvim.enable (default: true)
    - ghostty: apps.ghostty.enable (default: false)
      - When enabled: uses Nix programs.ghostty
      - When disabled: deploys custom config for Homebrew ghostty
    
    This pattern makes modules self-contained and toggleable.

--- modules/home-manager/apps/ideavim/ideavim.nix
+++ modules/home-manager/apps/ideavim/ideavim.nix
@@ -1,7 +1,21 @@
-_:
 {
-  xdg.configFile.ideavim = {
-    source = ./config;
-    recursive = true;
+  config,
+  lib,
+  ...
+}:
+{
+  options.apps.ideavim = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable IdeaVim configuration";
+    };
+  };
+
+  config = lib.mkIf config.apps.ideavim.enable {
+    xdg.configFile.ideavim = {
+      source = ./config;
+      recursive = true;
+    };
   };
 }
--- modules/home-manager/apps/nvim/nvim.nix
+++ modules/home-manager/apps/nvim/nvim.nix
@@ -1,15 +1,27 @@
-_:
 {
-  programs = {
-    neovim = {
+  config,
+  lib,
+  ...
+}:
+{
+  options.apps.nvim = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable Neovim configuration";
+    };
+  };
+
+  config = lib.mkIf config.apps.nvim.enable {
+    programs.neovim = {
       enable = true;
       defaultEditor = true;
       vimAlias = true;
     };
-  };
 
-  xdg.configFile.nvim = {
-    source = ./config;
-    recursive = true;
+    xdg.configFile.nvim = {
+      source = ./config;
+      recursive = true;
+    };
   };
 }

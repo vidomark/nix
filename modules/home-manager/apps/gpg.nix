cbfc1c996a41204da6b0f87a269895ae0e8739f0
vidomark42 <mark.vido@wise.com>
2025-12-08 20:40:42 +0100

    fix: revert GPG cache-ttl values to strings
    Integer values broke the build; GPG settings require string values.

--- modules/home-manager/apps/gpg.nix
+++ modules/home-manager/apps/gpg.nix
@@ -3,8 +3,8 @@ _:
   programs.gpg = {
     enable = true;
     settings = {
-      default-cache-ttl = 31536000; # 1 year
-      max-cache-ttl = 31536000; # 1 year
+      default-cache-ttl = "31536000"; # 1 year
+      max-cache-ttl = "31536000"; # 1 year
     };
   };
 }

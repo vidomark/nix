ffa1d3ece0fd421ee83ae094d2360c129196f3ed
vidomark42 <mark.vido@wise.com>
2025-12-11 19:14:28 +0100

    refactor: introduce profiles for composable configurations
    Added profiles/ directory with two composable profiles:
    - minimal.nix: Essential tools for any machine (includes karabiner)
    - development.nix: Full dev environment (imports minimal)
    
    This allows easy switching between configurations for different
    machines while keeping shared settings in one place.

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,38 +1,7 @@
 { username, homeDirectory, ... }:
 {
   imports = [
-    ./packages/core.nix
-    ./packages/fonts.nix
-
-    # Shell
-    ./packages/shell/zsh.nix
-
-    # Terminals
-    ./packages/terminals/ghostty/ghostty.nix
-    ./packages/terminals/alacritty.nix
-    ./packages/terminals/kitty.nix
-    ./packages/terminals/tmux.nix
-
-    # Development
-    ./packages/development/git.nix
-    ./packages/development/nvim/nvim.nix
-    ./packages/development/ideavim/ideavim.nix
-    ./packages/development/delta.nix
-    ./packages/development/rust-overlay.nix
-
-    # Window Management
-    ./packages/window-management/aerospace.nix
-    ./packages/window-management/jankyborders.nix
-
-    # Utilities
-    ./packages/utilities/bat.nix
-    ./packages/utilities/btop.nix
-    ./packages/utilities/gpg.nix
-    ./packages/utilities/yazi/yazi.nix
-    ./packages/utilities/starship.nix
-    ./packages/utilities/fzf.nix
-    ./packages/utilities/zoxide.nix
-    ./packages/utilities/karabiner/karabiner.nix
+    ./profiles/development.nix
   ];
 
   home = {

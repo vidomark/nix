80920496ecb1157455b3a309b58ccd3de0a695c8
vidomark42 <mark.vido@wise.com>
2026-04-08 17:38:44 +0200

    fix(nvim): remove broken jdtls override, enable notification wrap
    Remove jdtls.lua which used removed mason API get_install_path().
    LazyVim already handles lombok via $MASON/share/jdtls/lombok.jar.
    Enable text wrap in snacks.nvim notification history window.

--- modules/home-manager/packages/development/nvim/config/lua/plugins/snacks.lua
+++ modules/home-manager/packages/development/nvim/config/lua/plugins/snacks.lua
@@ -2,6 +2,11 @@ return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
+    styles = {
+      notification_history = {
+        wo = { wrap = true },
+      },
+    },
   },
   keys = {
     { "<leader>fr", false },
